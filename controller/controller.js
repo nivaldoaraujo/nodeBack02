32127a77cc8e9902b365f9b5d987c09688b258f3
Nivaldo Batista De Araujo <nbaraujo@firjan.com.br>
2026-03-12 14:08:43 -0300

    Primeiro commit do servidor 2

--- controller/controller.js
+++ controller/controller.js
@@ -2,5 +2,17 @@ const express = require("express")
 const app = express.Router()
 app.use(express.json())
 //rotas
+app.get("/usuario", () => {})
+
+app.get("/usuario/:id", () => {})
+
+app.post("/usuario", () => {})
+
+app.put("/usuario/:id", () => {})
+
+app.delete("/usuario/:id", () => {})
+
+
+
 
 module.exports = app
